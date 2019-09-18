7fa7d7fe49c7873f8c4191d97e1c310326e328e5
Sarthak <sgrover@protonmail.ch>
2019-09-18 22:11:47 +0530

    update to fix no update issue

--- update_github.sh
+++ update_github.sh
@@ -8,14 +8,17 @@ echo -e "Publish new public folder...\n"
 rm -r public/
 git submodule add -f -b master https://github.com/sarthakgrover/sarthakgrover.github.io.git public
 
+echo -e "Regenerate html code for display"
+hugo
+
 echo -e "Push to remote repository"
 git add .
 git commit -m "updated site"
 git push -u origin master
 
-echo -e "Regenerate html code for display"
-hugo
+echo -e "Push to public"
 cd public
 git add .
 git commit -m "Build website"
 git push origin master
+cd ..
